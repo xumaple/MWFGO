ff289f82d66933ba9b32647d98a7ac5058300a07
Wesley Chen <wesleylc@umich.edu>
2019-10-31 23:12:27 -0400

    created file tree for frontend

---
+++ __init__.py
@@ -0,0 +1,2 @@
+"""MWFGO package initializer"""
+
---
+++ config.py
@@ -0,0 +1,8 @@
+"""MWFGO development configuration."""
+
+import os
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+
+
