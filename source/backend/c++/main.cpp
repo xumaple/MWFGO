68982b1a735d184e57f9833e02061e6dd4405c85
Maple Xu <maplexu2010@gmail.com>
2019-11-01 20:20:25 -0400

    Added Catch test framework

--- source/backend/c++/main.cpp
+++ source/backend/c++/main.cpp
@@ -6,33 +6,6 @@
 #include "constraintManager.h"
 #include "weightLimiter.h"
 
-void tryAdd(Group &g, Individual &i) {
-    std::string s = g.addIndividual(i) ? "True" : "False";
-    std::cout << s << std::endl;
-}
-
 int main() {
-    ConstraintManager &m = ConstraintManager::getInstance();
-    std::string name = "schedule";
-    m.addConstraint(new TimeConstraint(), name);
-    std::vector<Individual> peoples;
-    peoples.emplace_back(new double(1.00000004));
-    peoples.emplace_back(new double(2.00000008));
-    peoples.emplace_back(new double(4.00000006));
-    
-    for (auto it = peoples.begin(); it != peoples.end(); ++it) {
-        auto nextIt = it;
-        for (++nextIt; nextIt != peoples.end(); ++nextIt) {
-            Individual::computeDiffs(*it, *nextIt);
-        }
-    }
-    std::vector<Limiter *> v(1, new WeightLimiter());
-    
-
-    Group g(v, &peoples[2]);
-
-    tryAdd(g, peoples[0]);
-    tryAdd(g, peoples[1]);
-
-    delete &m.getConstraint(name);
+    std::cout << "Hello world!\n";
 }
