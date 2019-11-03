cfc022dcc0cb197dab8e5d322075572c12bb7760
fyjiang <fyjiang@umich.edu>
2019-11-03 16:39:53 -0500

    Fixed api a bit

--- source/backend/restAPI/finalRes.py
+++ source/backend/restAPI/finalRes.py
@@ -6,26 +6,22 @@ import app
 @app.route('/api/result/', methods=["GET"])
 def get_result():
     # Get the limiters
-    limiters = getQuery("SELECT * FROM limiters", ())
+    limiters = getQuery("SELECT * FROM limiters")
 
-    # Get the leaders and constraints
-    leaders = getQuery("SELECT * FROM leaders", ())
-    l_constraints = []
-    for l in leaders:
-        l_constraints.append(getQuery("SELECT * FROM constrains WHERE person=?", (l['name'],)))
+    # Get the leaders
+    leaders = getQuery("SELECT * FROM leaders")
 
-    # Get the list of people to be organized and constraints
-    people = getQuery("SELECT * FROM people", ())
-    p_constraints = []
-    for p in people:
-        p_constraints.append(getQuery("SELECT * FROM constrains WHERE person=?", (p['name'],)))
+    # Get the list of people to be organized
+    people = getQuery("SELECT * FROM people")
+
+    # Get the list of constraints
+    constraints = getQuery("SELECT * FROM constraints")
 
     context = {
             'limiters': limiters,
             'leaders': leaders,
-            'leader_constraints': l_constraints,
             'people': people,
-            'people_constraints': p_constraints
+            'constraints': constraints
             }
 
     return flask.jsonify(**context)
