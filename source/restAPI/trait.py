4d9203379bdc6be1231c2eb6c8c00c7caf0c8209
Wesley Chen <wesleylc@umich.edu>
2019-11-04 09:15:38 -0500

    test

---
+++ source/restAPI/trait.py
@@ -0,0 +1,44 @@
+import flask
+import api
+
+@insta485.app.route('/api/organizer/traits/<int:traitid_url_slug>/',
+                    methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def get_trait(traitid_url_slug):
+    """Get Trait."""
+
+    # Get POST request.
+    if flask.request.method == 'POST':
+
+        context = {
+            "name": "Frank",
+            "isConstraint": False,
+            "formType": 2,
+        }
+        return flask.jsonify(**context), 201
+    
+    # Get DELETE request.
+    if flask.request.method == 'DELETE':
+
+        context = {
+        }
+        return flask.jsonify(**context), 204
+
+    # Get PATCH request.
+    if flask.request.method == 'PATCH':
+
+        context = {
+            "name": "Frank",
+            "isConstraint": False,
+            "formType": 2,
+        }
+        return flask.jsonify(**context), 204
+
+    # GET request response.
+
+    context = {
+        "id": 1,
+        "name": "Maple Xu",
+        "isConstraint": False,
+        'formType': 2,
+    }
+    return flask.jsonify(**context)
