5e33afa688060a433efc749af06c74f46f1efef4
Wesley Chen <wesleylc@umich.edu>
2019-11-04 10:50:40 -0500

    changed __init__.py outside of restAPI

--- source/api/restAPI/traits.py
+++ source/api/restAPI/traits.py
@@ -1,14 +1,14 @@
 import flask
 import api
 
-@api.app.route('/restAPI/organizer/traits/',
+@api.app.route('/api/v1/organizer/traits/',
                     methods=['GET'])
 def get_traits():
     """Get Traits."""
-
+    print("hihihihi")
     # GET request response.
 
     context = {
-        'traits': [{id: 1}, {id: 2}, {id: 3}]
+        'traits': [{'id': 1}, {'id': 2}, {'id': 3}]
     }
     return flask.jsonify(**context)
