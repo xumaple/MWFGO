f8cd2d0f3314a41e40c6dc22b39200c7b67f302d
fyjiang <fyjiang@umich.edu>
2019-11-19 09:22:50 -0500

    Worked on memberSurvey

--- source/api/restAPI/memberSurvey.py
+++ source/api/restAPI/memberSurvey.py
@@ -5,16 +5,28 @@ import os, sys
 
 @api.app.route('/api/v1/member/', methods = ['GET'])
 def get_member_form():
+    req_data = flask.request.get_json()
     # Get the list of traits
-    # Set member_id
-    member_id = None
+    # Set member_id (fix with correct data later)
+    member_id = req_data['hash']
     # Hit the members table for the list of traits
-    # db.session.query(tables["Members_" + event_id]).filter_by(id=member_id).one().__dict__
+    result =  db.session.query(tables["Members_" + event_id]).filter_by(id=member_id).one().__dict__
 
+    # Hit the results table for the questions and choices
+    questions = list(db.session.query(tables["Traits_" + event_id]))
+
+    # Find some way to combine the questions with the appropriate answers from the member
+    
     # Return the Question and choices for each trait
+    flask.jsonify(**result)
 
 @api.app.route('/api/v1/member/', methods = ['POST'])
 def post_member_form():
     # Create an entry in the members table for this member with all the data
     req_data = flask.request.get_json()
+
+    # new_mem = Members(add arguments in here )
+    # db.session.add(new_mem)
+    # db.session.commit()
+
     return flask.make_response("", 201)
