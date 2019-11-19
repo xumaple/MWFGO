53d49d03a85101b81d8cb15a9e9a50b22c13cede
fyjiang <fyjiang@umich.edu>
2019-11-18 21:22:53 -0500

    Working on memberSurvey

--- source/api/restAPI/memberSurvey.py
+++ source/api/restAPI/memberSurvey.py
@@ -2,32 +2,19 @@ import flask
 import api
 import os, sys
 
-@api.app.route('/api/v1/member/', methods=['POST'])
-def add_member():
-    # Get the string for the name
 
-    # Hash the name
-    hashed_name = None
-
-    # Return the url
-    res = {
-        'newUrl': '/api/v1/member/' + hashed_name
-    }
-
-    return flask.jsonify(**res)
-
-@api.app.route('/api/v1/member/<string:hashed_name>', methods = ['GET'])
-def get_member_form(hashed_name):
+@api.app.route('/api/v1/member/', methods = ['GET'])
+def get_member_form():
     # Get the list of traits
+    # Set member_id
+    member_id = None
+    # Hit the members table for the list of traits
+    # db.session.query(tables["Members_" + event_id]).filter_by(id=member_id).one().__dict__
 
     # Return the Question and choices for each trait
 
-@api.app.route('/api/v1/member/<string:hashed_name>', methods = ['POST'])
-def post_member_form(hashed_name):
+@api.app.route('/api/v1/member/', methods = ['POST'])
+def post_member_form():
     # Create an entry in the members table for this member with all the data
+    req_data = flask.request.get_json()
     return flask.make_response("", 201)
-
-@api.app.route('/api/v1/member/<string:hashed_name>', methods = ['PATCH'])
-def patch_member_form(hashed_name):
-    # Depending on what the user changed, change the according value in the table
-    return flask.make_response("", 200)
