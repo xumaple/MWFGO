1aeee2712dfcf33021d3c10a81744c7f5bb3a630
Maple Xu <maplexu2010@gmail.com>
2019-11-20 10:53:32 -0500

    Working on getting it to work...

--- source/api/model/model.py
+++ source/api/model/model.py
@@ -1,14 +1,24 @@
 """Database API."""
 
-from api import app
+from api import app, db_uri
 from flask_sqlalchemy import SQLAlchemy
 # Configure MySQL connection to Flask app
 db = SQLAlchemy(app)
+# engine = SQLAlchemy.create_engine(db_uri)
 
 event_id = "0"
 trait_id = "0"
 
 tables = {}
+
+# class Table(db.Model):
+#     def __init__(self):
+#         self.deleted = False
+
+#     def drop_table(self):
+#         if not self.deleted:
+#             self.__table__.drop(db.get_engine())
+#             self.deleted = True
 
 class Event(db.Model):
     __tablename__ = 'events'
@@ -60,28 +70,28 @@ class Nonconstraints(db.Model):
 tables["nonconstraints_0"] = Nonconstraints
 
 #not actually gonna be here once restAPI is done
-class Members(db.Model):
-    __tablename__ = 'members_' + event_id
+# class Members(db.Model):
+#     __tablename__ = 'members_' + event_id
 
-    id = db.Column(db.String(16), primary_key=True, nullable=False)
-    name = db.Column(db.String(256), nullable=False)
-    non_constraints = db.relationship(Nonconstraints, backref='members', lazy=True, cascade="all, delete-orphan")
+#     id = db.Column(db.String(16), primary_key=True, nullable=False)
+#     name = db.Column(db.String(256), nullable=False)
+#     non_constraints = db.relationship(Nonconstraints, backref='members', lazy=True, cascade="all, delete-orphan")
 
-    def __repr__(self):
-        return f"members('{self.id}', '{self.name}', '{self.trait_}')"
-tables["members_" + event_id] = Members
+#     def __repr__(self):
+#         return f"members('{self.id}', '{self.name}', '{self.trait_}')"
+# tables["members_" + event_id] = Members
 
-#not actually gonna be here once restAPI is done
-class Leaders(db.Model):
-    __tablename__ = 'leaders_' + event_id
+# #not actually gonna be here once restAPI is done
+# class Leaders(db.Model):
+#     __tablename__ = 'leaders_' + event_id
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(256), nullable=False)
-    non_constraints = db.relationship(Nonconstraints, backref='leaders', lazy=True, cascade="all, delete-orphan")
+#     id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     name = db.Column(db.String(256), nullable=False)
+#     non_constraints = db.relationship(Nonconstraints, backref='leaders', lazy=True, cascade="all, delete-orphan")
 
-    def __repr__(self):
-        return f"choices('{self.id}', '{self.name}', '{self.trait_}')"
-tables["leaders_" + event_id] = Leaders
+#     def __repr__(self):
+#         return f"choices('{self.id}', '{self.name}', '{self.trait_}')"
+# tables["leaders_" + event_id] = Leaders
 
 #not actually gonna be here once restAPI is done
 class Traits(db.Model):
@@ -100,5 +110,6 @@ class Traits(db.Model):
         return f"traits('{self.id}', '{self.name}', '{self.question}', '{self.is_constraint}', '{self.form_type}', '{self.context}')"
 tables["traits_" + event_id] = Traits
 
+db.metadata.bind = db.get_engine()
 db.create_all()
 db.session.commit()
--- source/api/restAPI/__init__.py
+++ source/api/restAPI/__init__.py
@@ -1,6 +1,6 @@
 """Insta485 REST API."""
 
 from api.restAPI.traits import *
-from api.restAPI.member import *
+from api.restAPI.memberSurvey import *
 from api.restAPI.leaderSurvey import *
 from api.restAPI.organizer import create_member_table
